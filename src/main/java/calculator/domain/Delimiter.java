1f349d681563f5bbcd7208138cf3ae282286e084
sunwon <jsw5913@naver.com>
2025-10-20 18:49:59 +0900

    refactor: 개행 문자는 커스텀 구분자로 사용 금지

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -9,6 +9,9 @@ public record Delimiter(String value) {
         if (value == null || value.isEmpty()) {
             throw new IllegalArgumentException("커스텀 구분자는 비어있을 수 없습니다.");
         }
+        if (value.contains("\n")) {
+            throw new IllegalArgumentException("커스텀 구분자에 개행 문자를 포함할 수 없습니다.");
+        }
         if (isNumeric(value)) {
             throw new IllegalArgumentException("커스텀 구분자는 숫자로만 구성될 수 없습니다.");
         }
