c011d2880bda801a8b86a095c4f5b599d788578e
sunwon <jsw5913@naver.com>
2025-10-20 16:17:35 +0900

    feat: 문자열 입력 받기

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,9 +1,18 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
-    public void printInputPrompt() {
+    public String readInput() {
         System.out.println(INPUT_PROMPT_MESSAGE);
+        String input = Console.readLine();
+
+        if (input == null) {
+            throw new IllegalArgumentException("입력값이 null입니다.");
+        }
+
+        return input;
     }
 }
